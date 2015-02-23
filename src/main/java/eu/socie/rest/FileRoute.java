fb45e7e1c4eab4634f717bc3792a2ecaefad2fb9
Bram Wiekens <wiekens@tizin.nl>
2015-02-23 11:14:54 +0100

    Content type and Content disposition set on file return

--- src/main/java/eu/socie/rest/FileRoute.java
+++ src/main/java/eu/socie/rest/FileRoute.java
@@ -10,6 +10,8 @@ import com.jetdrone.vertx.yoke.core.YokeFileUpload;
 import com.jetdrone.vertx.yoke.middleware.YokeRequest;
 import com.jetdrone.vertx.yoke.middleware.YokeResponse;
 
+import eu.socie.mongo_async_persistor.util.MongoFileUtil;
+
 public class FileRoute extends Route {
 
 	public FileRoute(String path, Vertx vertx) {
@@ -20,52 +22,89 @@ public class FileRoute extends Route {
 	}
 
 	private void handleFilePost(YokeRequest request) {
-	
+
 		if (request.files() != null) {
-			request.files().forEach((key,upload) -> processFile(request, key,upload));
+			request.files().forEach(
+					(key, upload) -> processFile(request, key, upload));
 		}
 
 	}
-	
-	private void processFile(YokeRequest request, String key, YokeFileUpload upload) {
+
+	private void processFile(YokeRequest request, String key,
+			YokeFileUpload upload) {
 		// TODO consider reading files in chunks, to enable large file support
-		vertx.fileSystem().readFile(upload.path(), result -> handleFileRead(request, upload, result));
+		vertx.fileSystem().readFile(upload.path(),
+				result -> handleFileRead(request, upload, result));
 	}
-	
-	private void handleFileRead(YokeRequest request, YokeFileUpload upload, AsyncResult<Buffer> fileBuffer){
-		mongoHelper.sendStoreFile(upload.filename(), upload.contentType(), fileBuffer.result(), r -> printId(request, r));
-		
+
+	private void handleFileRead(YokeRequest request, YokeFileUpload upload,
+			AsyncResult<Buffer> fileBuffer) {
+		mongoHelper.sendStoreFile(upload.filename(), upload.contentType(),
+				fileBuffer.result(), r -> printId(request, r));
+
 		upload.delete();
 	}
-	
-	private void printId(YokeRequest request, AsyncResult<Message<String>> fileStoreResult){
+
+	private void printId(YokeRequest request,
+			AsyncResult<Message<String>> fileStoreResult) {
 		YokeResponse response = request.response().setChunked(true);
-		
+
 		if (fileStoreResult.succeeded()) {
-			
+
 			response.write(fileStoreResult.result().body());
 		}
-		
+
 		response.end();
 	}
 
+	private boolean checkDownload(String download){
+		boolean isDownload = false;
+		
+		if (download != null) {
+			if (download.equalsIgnoreCase("true") || download.equalsIgnoreCase("false")){
+				isDownload = new Boolean(download);
+			}
+		}
+		
+		return isDownload;
+	}
+	
 	private void handleFileGet(YokeRequest request) {
 		String fileId = request.getParameter("fileId");
+		String download =	request.getParameter("download");
+		final boolean isDownload = checkDownload(download);
+				
 		JsonObject fileMsg = new JsonObject();
-		
+
 		fileMsg.putString("_id", fileId);
 
 		mongoHelper.sendGetFile(fileMsg,
-				fileRequest -> handleFileResponse(fileRequest, request));
+				fileRequest -> handleFileResponse(fileRequest, request, isDownload));
 
 	}
 
 	private void handleFileResponse(AsyncResult<Message<Buffer>> fileResult,
-			YokeRequest request) {
+			YokeRequest request, boolean isDownload) {
 		if (fileResult.succeeded()) {
+			String disposition = isDownload ? "attachtment" : "inline";
+			YokeResponse response = request.response().setChunked(true);
 
 			Message<Buffer> msg = fileResult.result();
-			request.response().setChunked(true).write(msg.body()).end();
+
+			Buffer fileBuffer = msg.body();
+
+			String filename = MongoFileUtil.getFilenameFromBuffer(fileBuffer);
+			String contentType = MongoFileUtil
+					.getContentFileFromBuffer(fileBuffer);
+
+			response.headers().add("Content-Disposition",
+					String.format("%s; filename=\"%s\"", disposition, filename));
+
+			response.setContentType(contentType);
+			
+			response.write(MongoFileUtil.getFileContentsFromBuffer(fileBuffer));
+
+			response.end();
 		} else {
 			String errMsg = fileResult.cause().getMessage();
 			request.response().setStatusCode(Route.ERROR_CLIENT_NOT_FOUND)
